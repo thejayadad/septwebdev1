af28c23ebee2e93b78836f5acb274193c4e065e9
thejayadad <thejayadad@gmail.com>
2023-10-01 06:09:20 -0400

    update

---
+++ nav/main.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById('menuIcon');
+const menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', () => {
+    if (menu.style.left === '-250px') {
+        menu.style.left = '0';
+        menuIcon.innerHTML = '<i class="fas fa-times"></i>';
+    } else {
+        menu.style.left = '-250px';
+        menuIcon.innerHTML = '<i class="fas fa-bars"></i>';
+    }
+});
