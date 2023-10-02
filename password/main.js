acac3aeb9ab9a989224657f8ea7857d2ceb00da3
thejayadad <thejayadad@gmail.com>
2023-10-02 13:41:14 -0400

    up

--- password/main.js
+++ password/main.js
@@ -1,12 +1,12 @@
-function generatePassword() {
+
+function generatepassword(){
     const length = 12;
     const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
     let password = "";
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * letters.length);
-        password += letters[randomIndex];
+    for (let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * letters.length)
+        password += letters[randomIndex]
     }
-
-    const passwordElement = document.getElementById("password");
+    const passwordElement = document.getElementById('password')
     passwordElement.textContent = password;
 }
