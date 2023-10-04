a369af4b1da5002b57abde5e185eb6e05161b56d
thejayadad <thejayadad@gmail.com>
2023-10-04 07:52:36 -0400

    update

---
+++ picker/main.js
@@ -0,0 +1,5 @@
+function changeBackgroundColor() {
+    const colorPicker = document.getElementById('colorPicker');
+    const container = document.querySelector('.container');
+    container.style.backgroundColor = colorPicker.value;
+}
