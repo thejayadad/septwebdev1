87d290c28934b4991e07a5d8be698846459511d7
thejayadad <thejayadad@gmail.com>
2023-10-04 11:29:43 -0400

    update

---
+++ table/main.js
@@ -0,0 +1,18 @@
+const data = [
+    { Name: "Jace", Age: 16, Grade: 10 },
+    { Name: "Jazz", Age: 13, Grade: 8 },
+    { Name: "Jada", Age: 13, Grade: 8 },
+    { Name: "Jack", Age: 10, Grade: 5 }
+
+];
+
+const table = document.getElementById("myTable");
+const tbody = table.getElementsByTagName("tbody")[0];
+
+data.forEach(item => {
+    const row = tbody.insertRow();
+    Object.values(item).forEach(value => {
+        const cell = row.insertCell();
+        cell.appendChild(document.createTextNode(value));
+    });
+});
